a198499fb7b394273f2bdece22bfd99db5e97aa5
Leo Loureiro <leolou.90@gmail.com>
2022-12-30 21:11:12 -0700

    Display transactions in ul

--- src/components/History.js
+++ src/components/History.js
@@ -1,12 +1,19 @@
 
 
-const History = () =>{
+const History = (props) =>{
+    const transactions = props.transactions
+
+    const listTransactions = transactions.map((transaction) =>
+        <li>
+            <span>{transaction.transactionText}</span> <span>{transaction.transactionQuantity}</span>
+        </li>
+    )
     return(
         <div>
             <h2>History</h2>
             <hr></hr>
             <ul className='transaction-list'>
-
+                {listTransactions}
             </ul>
         </div>
     )
